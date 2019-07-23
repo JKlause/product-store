68e4fc14ffb8f93672471a7129cff97f34e18bce
Joe Klause <joeklause@gmail.com>
2019-07-23 09:51:23 -0700

    a few edits

--- src/render-sandwich.js
+++ src/render-sandwich.js
@@ -32,13 +32,3 @@ function renderSandwich(sandwich) {
     return li;
 
 }
-
-// const applePie = {
-//     code: 'apple-pie',
-//     name: 'Apple Pie',
-//     image: 'assets/apple-applePie.jpg',
-//     description: 'Delicious Apple Pie Filling with Salted Caramel, Vanilla Ice Cream, and Cinnamon Shortbread Cookies.',
-//     category: 'cinnamon-cookie',
-//     price: 6.00,
-//     cost: 2.30,
-// };
