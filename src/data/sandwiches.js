41fc319816857b9382b4d0791f04c9d8427988e0
Joe Klause <joeklause@gmail.com>
2019-07-22 18:04:05 -0700

    written out list items and matrix

--- src/data/sandwiches.js
+++ src/data/sandwiches.js
@@ -6,6 +6,87 @@ const applePie = {
     category: 'cinnamon-cookie',
     price: 6.00,
     cost: 2.30,
-}
+};
 
-const 
+const bananaCream = {
+    code: 'banana-cream',
+    name: 'Banana Cream',
+    image: 'assets/banana-cream.jpg',
+    description: 'Creamy Banana Pie Filling with Custard, Vanilla Ice Cream, and Graham Cracker Shortbread Cookies.',
+    category: 'graham-cracker-cookie',
+    price: 6.00,
+    cost: 2.00,
+};
+
+const cookieDough = {
+    code: 'cookie-dough',
+    name: 'Cookie Dough',
+    image: 'assets/cookie-dough.jpg',
+    description: 'Cookie Dough Pieces with Chocolate Mousse, Vanilla Ice Cream, and Chocolate Shortbread Cookies.',
+    category: 'chocolate-cookie',
+    price: 8.00,
+    cost: 3.00,
+};
+
+const keyLimePie = {
+    code: 'key-lime-pie',
+    name: 'Key Lime Pie',
+    image: 'assets/key-lime-pie.jpg',
+    description: 'Tangy Key Lime Pie filling, Vanilla Ice Cream, and Cinnamon Shortbread Cookies.',
+    category: 'cinnamon-cookie',
+    price: 7.50,
+    cost: 2.25,
+};
+
+const pecanPie = {
+    code: 'pecan-pie',
+    name: 'Pecan Pie',
+    image: 'assets/pecan-pie.jpg',
+    description: 'Pecan Pie Filling made with Maple Syrup, Vanilla Ice Cream, and Regular Shortbread Cookies.',
+    category: 'regular-cookie',
+    price: 7.50,
+    cost: 3.00,
+};
+
+const saltedCaramelBrownie = {
+    code: 'salted-caramel-brownie',
+    name: 'Salted Caramel Brownie',
+    image: 'assets/salted-caramel-brownie.jpg',
+    description: 'Fudgy Brownie Pieces with Salted Caramel, Vanilla Ice Cream, and Chocolate Shortbread Cookies.',
+    category: 'chocolate-cookie',
+    price: 8.00,
+    cost: 3.00,
+};
+
+const smores = {
+    code: 'smores',
+    name: 'S\'mores',
+    image: 'assets/smores.jpg',
+    description: 'Toasted Marshmallows with Chocolate Mousse, Chocolate Ice Cream, and Graham Cracker Shortbread Cookies.',
+    category: 'graham-cracker-cookie',
+    price: 7.00,
+    cost: 2.50,
+};
+
+const strawberryShortcake = {
+    code: 'strawberry-shortcake',
+    name: 'Strawberry Shortcake',
+    image: 'assets/strawberry-shortcake.jpg',
+    description: 'Vanilla Mousse, Strawberry Ice Cream, and Regular Shortbread Cookies.',
+    category: 'regular-cookie',
+    price: 7.00,
+    cost: 2.25,
+};
+
+const sandwiches = [
+    saltedCaramelBrownie,
+    cookieDough,
+    pecanPie,
+    keyLimePie,
+    smores,
+    strawberryShortcake,
+    bananaCream,
+    applePie,
+];
+
+export default sandwiches;
