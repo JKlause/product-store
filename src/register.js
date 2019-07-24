9da0c218d0ee8cdcaacaf782554c6077babf4805
Joe Klause <joeklause@gmail.com>
2019-07-23 17:55:37 -0700

    working tests

---
+++ src/register.js
@@ -0,0 +1,28 @@
+export function findProduct(sandwiches, code) {
+    //loop through array
+    for(let i = 0; i < sandwiches.length; i++) {
+        const sandwich = sandwiches[i];
+
+        if(sandwich.code === code) {
+            return sandwich;
+        } 
+    }
+    return null;
+}
+
+export function getLineTotal(quantity, price) {
+    return (quantity * price);
+}
+
+export function calcOrderTotal(cart, sandwiches) {
+
+    let orderTotal = 0;
+
+    for(let i = 0; i < cart.length; i++) {
+        const sandwichPrice = (findProduct(sandwiches, cart[i].code)).price;
+        const quantity = cart[i].quantity;
+        const lineTotal = getLineTotal(quantity, sandwichPrice);
+        orderTotal += +lineTotal;
+    }
+    return orderTotal;
+}
