a38c742c8c3dba7e9d8219c8bd8c6308abd237ba
Joe Klause <joeklause@gmail.com>
2019-07-23 22:13:41 -0700

    working html test

--- src/render-line-items.js
+++ src/render-line-items.js
@@ -13,12 +13,13 @@ function renderLineItem(lineItem, sandwich) {
     quantityCell.textContent = lineItem.quantity;
     tr.appendChild(quantityCell);
 
-    
+    const priceCell = document.createElement('td');
+    priceCell.textContent = toUSD(sandwich.price);
+    tr.appendChild(priceCell);
 
-    
-    
-
-
+    const lineTotalCell = document.createElement('td');
+    lineTotalCell.textContent = toUSD(getLineTotal(lineItem.quantity, sandwich.price));
+    tr.appendChild(lineTotalCell);
 
     return tr;
 }
