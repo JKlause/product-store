ef0f8f9c100a09be9bb2fd12864760527f6dc4a5
Joe Klause <joeklause@gmail.com>
2019-07-30 09:20:19 -0700

    still functional, not refactoring while loop since it seems to mess it up worse

--- src/shopping-cart.js
+++ src/shopping-cart.js
@@ -1,4 +1,3 @@
-import sandwiches from './data/sandwiches.js';
 import { calcOrderTotal } from './register.js';
 import store from './data/store.js';
 import { renderNoLineItem, renderLineItemWithRemove } from './render-line-items.js';
@@ -54,10 +53,10 @@ function updateOrderTotal() {
 }
 
 function renderOrderTotal(discount) {
+    const sandwiches = store.getProducts();
     orderTotalCell.textContent = toUSD(calcOrderTotal(shoppingCart, sandwiches, discount));
 }
 
-//fix the reference to sandwiches to products in renderOrderTotal
 
 function validateAndApplyPromoDiscount() {
     const promoCode = customerPromoCode.value;
