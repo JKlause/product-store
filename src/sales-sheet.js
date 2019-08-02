7f7610c036d73e1f73a3d19ad86359ee5561fdaa
Joe Klause <joeklause@gmail.com>
2019-08-02 16:43:48 -0700

    sales sheet totals

--- src/sales-sheet.js
+++ src/sales-sheet.js
@@ -1,18 +1,48 @@
 import renderSalesSheetLineItem from './render-sales-sheet-line-item.js';
 import store from './data/store.js';
+import { toUSD } from './format-dollar.js';
+import { getRevenueLineItemTotal, getProfitLineItemTotal } from './register.js';
+import sandwiches from './data/sandwiches.js';
 
 const salesSheetTableBody = document.getElementById('sales-sheet-body');
+const totalProductsSoldCell = document.getElementById('total-products-sold-cell');
+const totalCostCell = document.getElementById('total-cost-cell');
+const totalRevenueCell = document.getElementById('total-revenue-cell');
+const totalProfitCell = document.getElementById('total-profit-cell');
+
+let totalProductsSold = 0;
+let totalCost = 0;
+let totalRevenue = 0;
+let totalProfit = 0;
 
 
-while(salesSheetTableBody.firstChild) {
-    salesSheetTableBody.removeChild(salesSheetTableBody.firstChild);
+clearOldSalesSheet();
+
+generateNewSalesSheet();
+
+function clearOldSalesSheet() {
+    while(salesSheetTableBody.firstChild) {
+        salesSheetTableBody.removeChild(salesSheetTableBody.firstChild);
+    }
 }
 
-const salesSheetArray = store.getSales();
-for(let i = 0; i < salesSheetArray.length; i++) {
-    const product = store.getProduct(salesSheetArray[i].code);
-    const quantity = salesSheetArray[i].quantity;
-    const dom = renderSalesSheetLineItem(quantity, product);
-    salesSheetTableBody.appendChild(dom);
+function generateNewSalesSheet() {
+    const salesSheetArray = store.getSales();
+    for(let i = 0; i < salesSheetArray.length; i++) {
+        const product = store.getProduct(salesSheetArray[i].code);
+        const quantity = salesSheetArray[i].quantity;
+        const dom = renderSalesSheetLineItem(quantity, product);
+        salesSheetTableBody.appendChild(dom);
+
+        totalProductsSold = totalProductsSold + +salesSheetArray[i].quantity;
+        totalCost = totalCost + product.cost;
+        totalRevenue = totalRevenue + getRevenueLineItemTotal(quantity, product.price, product.cost);
+        totalProfit = totalProfit + getProfitLineItemTotal(quantity, product.price, product.cost);
+    }
+
+    totalProductsSoldCell.textContent = totalProductsSold;
+    totalCostCell.textContent = toUSD(totalCost);
+    totalRevenueCell.textContent = toUSD(totalRevenue);
+    totalProfitCell.textContent = toUSD(totalProfit);
 }
-//i need to get 
+
